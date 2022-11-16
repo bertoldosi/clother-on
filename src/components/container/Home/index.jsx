e836ce8eb0ed6713b5cd561b75516063c54ffaf3
bertoldosi <matheus.bertoldosi@gmail.com>
2022-11-16 15:58:40 -0300

    implentando styled-components

--- src/components/container/Home/index.jsx
+++ src/components/container/Home/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Container } from "./styles";
 
 const Home = () => {
+  const status = false;
   return (
-    <>
+    <Container status={status}>
       <header>Header</header>
       <section>banner</section>
       <section>itens</section>
       <footer>footer</footer>
-    </>
+    </Container>
   );
 };
 
