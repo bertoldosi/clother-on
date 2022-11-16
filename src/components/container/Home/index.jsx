21d85fd22a789d2f358affdd96aa31f506a77595
bertoldosi <matheus.bertoldosi@gmail.com>
2022-11-16 15:15:09 -0300

    estrutura inicial

---
+++ src/components/container/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <header>Header</header>
+      <section>banner</section>
+      <section>itens</section>
+      <footer>footer</footer>
+    </>
+  );
+};
+
+export default Home;
